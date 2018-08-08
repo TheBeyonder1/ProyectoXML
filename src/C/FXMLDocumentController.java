1511a56869ef3344f12473e5bb139bda3e07d158
santiago.arce <santiago.arce@SCG-02V-D58726.uao.edu.co>
2018-08-08 14:58:52 -0500

    Hola texto3

--- src/C/FXMLDocumentController.java
+++ src/C/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package C;
 
 import Modelo.Empleado;
@@ -16,22 +11,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-/**
- *
- * @author santiago.arce
- */
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private TextField textoNombre;
+    private TextField textNombre;
     @FXML
-    private TextField textoCodigo;
+    private TextField textCodigo;
    @FXML
-    private TextField textoEdad;
+    private TextField textEdad;
     @FXML
-    private TextField textoDireccion;
+    private TextField textDireccion;
     @FXML
-    private TextField textoDepto;
+    private TextField textDepto;
     @FXML
     private Label lnombre;
     @FXML
@@ -42,21 +33,21 @@ public class FXMLDocumentController implements Initializable {
     private Label ldireccion;
     @FXML
     private Label ldepto;
-    @FXML
-    private Button botonGuardar;
-    @FXML
-    private Button botonxml;
+//    @FXML
+//    private Button botonGuardar;
+//    @FXML
+//    private Button botonxml;
     LinkedList<Empleado>Lista;
 
       
     @FXML
     private void AgregarLista(ActionEvent event) {
       
-       String nom=textoNombre.getText();
-       String dir=textoDireccion.getText();
-       int edad=Integer.parseInt(textoEdad.getText());
-       String cod=textoCodigo.getText();
-       String depto=textoDepto.getText();
+       String nom=textNombre.getText();
+       String dir=textDireccion.getText();
+       int edad=Integer.parseInt(textEdad.getText());
+       String cod=textCodigo.getText();
+       String depto=textDepto.getText();
         
        Empleado empleado=new Empleado (cod,depto,nom,edad,dir);
        Lista.add(empleado);
